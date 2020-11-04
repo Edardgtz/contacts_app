19de77a1acf0a4c80cc05f9581098472295efacc
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-03 20:36:56 -0600

    added route, first_contact method to list contact info for first contact, added all_contact method(no logic yet) created firs_contact.json.jb and all_contacts.json.jb files

--- app/controllers/api/contact_pages_controller.rb
+++ app/controllers/api/contact_pages_controller.rb
@@ -1,3 +1,23 @@
 class Api::ContactPagesController < ApplicationController
+  def first_contact
+    @contact = Contact.first
+    render "first_contact.json.jb"
+  end
+
+  def total_contacts
+    @contact1 = Contact.first
+    @contact2 = Contact.second
+    group_contacts = []
+    group_contacts << @contact1
+    group_contacts << @contact2
+    i = 0
+    while i < group_contacts.length
+      group_contacts[i]
+      i += 1
+    end
+
+    render "all_contacts.json.jb"
+    
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/contacts" => "contact_pages#first_contact"
+  end
+
+  namespace :api do
+    get "/all_contacts" => "contact_pages#total_contacts"
+  end
 end
